3ac9ab59e1529a7f10e99c6b348f1ca9cdb3c1c5
Aitana Shough <aeshough@gmail.com>
2022-11-02 14:20:27 -0700

    WIP: add UI logic and event listeners
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 
-// Business Logic
+// Business Logic -----------------------------------
 
 // Business Logic for Game Board
 function GameBoard() {
   this.players = {};
   this.id = 0;
-  this.turn = 0;
+  this.turnId = 0;
 }
 
 GameBoard.prototype.addPlayer = function(players) {
@@ -35,37 +35,85 @@ function Player(name) {
   this.turnScore = 0;
 }
 
-
+// Game Turn Function
 function newTurn(hold, player) {
   let dice = new Dice();
 
-  if(!hold) {
+  if (!hold) {
     let roll = dice.roll();
-    if(roll === 1)
-    {
+    if (roll === 1) {
       console.log("dice roll === 1");
       player.turnScore = 0;
+      if(gameBoard.turnId <= Object.keys(gameBoard.players).length) {
+        gameBoard.turnId += 1;
+      }
+      else {
+        gameBoard.turnId = 1;
+      }      
       return 0;
-    }
-    else {
+    } else {
       player.turnScore += roll;
       console.log("player scores! " + player.turnScore);
-
     }
-  }
-  else {
+  } else {
     console.log("Turn Score: " + player.turnScore);
     player.gameScore += player.turnScore;
     console.log("turn end, player held: " + player.gameScore);
     player.turnScore = 0;
+    gameBoard.turnId += 1;
     return 0;
   }
 
-
-  if(player.gameScore >= 100) {
+  if (player.gameScore >= 100) {
     console.log("score is 100," + player +" wins!");
     return 0;
   }
-  console.log(Game Score: player.gameScore "Turn Score: ");
+  console.log("Game Score:" + player.gameScore + "Turn Score: " + player.turnScore);
 }
 
+// UI Logic -----------------------------------
+let gameBoard = new GameBoard();
+
+function handleGameStart() {
+  e.preventDefault;
+// const addPlayer = document.querySelector("input#player-add")  
+  const player1Input = document.querySelector("input#player1").value;
+  const player2Input = document.querySelector("input#player2").value;
+  let player1 = new Player(player1Input);
+  let player2 = new Player(player2Input);
+  gameBoard.addPlayer(player1);
+  gameBoard.addPlayer(player2);
+  gameBoard.turnId = 1;
+  const holdButton = document.querySelector("button.hold");
+  const rollButton = document.querySelector("button.roll");
+  newTurn(false, player1); // run first turn: player 1 rolls die
+  // then we branch depending on which option player1 picks
+
+  holdButton.removeAttribute("disabled");
+  rollButton.removeAttribute("disabled");
+ 
+
+//  p>Player: <span id="player-name"> </p>
+//  <p>Turn Score: <span id="turn-score"> </p>
+//  <p>Current Score: <span id="current-score"> </p>
+//  <p>Roll: <span id="dice-roll"> </p>
+
+}
+
+function handleHold() {
+  e.preventDefault;
+  let player = gameBoard.players[gameBoard.turnId];  
+  newTurn(true, player);
+}
+
+function handleRoll() {
+  e.preventDefault;
+  let player = gameBoard.players[gameBoard.turnId];
+  newTurn(false, player);
+}
+
+window.addEventListener("load", function () {
+  document.querySelector("form#form").addEventListener("submit", handleGameStart);
+  document.querySelector("button.hold").addEventListener("click", handleHold);
+  document.querySelector("button.roll").addEventListener("click", handleRoll);
+});
